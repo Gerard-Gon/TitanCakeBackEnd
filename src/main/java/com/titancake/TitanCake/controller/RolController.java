56911736a90ce27456e188fa364e76de80f89ce6
POAPCA02LC0626 <SSDD@POAPCA02LC0626.duoc.acad>
2025-11-18 17:43:26 -0300

    Guardando RolController

---
+++ src/main/java/com/titancake/TitanCake/controller/RolController.java
@@ -0,0 +1,51 @@
+package com.titancake.TitanCake.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.titancake.TitanCake.model.Rol;
+import com.titancake.TitanCake.service.RolService;
+
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@RequestMapping("/api/v1/roles")
+public class RolController {
+
+    @Autowired
+    private RolService rolService;
+
+    @GetMapping
+    public ResponseEntity<List<Rol>> getAllRol() {
+        List<Rol> roles = rolService.findAll();
+        if (roles.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(roles);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Rol> getRolById(@PathVariable Integer id) {
+        Rol rol = rolService.findById(id);
+        if (rol == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(rol);
+    }
+
+    @PostMapping
+    public ResponseEntity<Rol> createRol(@RequestBody Rol rol) {
+        Rol createdRol = rolService.save(rol);
+        return ResponseEntity.status(201).body(createdRol);
+    }
+
+}
