b93251446e53929dbf76753c71d395427f14241c
Gerard-Gon <gera.gonzalezs@duocuc.cl>
2025-11-22 03:32:25 -0300

    Eliminamos recetas

--- src/main/java/com/titancake/TitanCake/repository/RecetaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.titancake.TitanCake.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.titancake.TitanCake.model.Receta;
-
-public interface RecetaRepository extends JpaRepository<Receta, Integer>  {
-    
-}
